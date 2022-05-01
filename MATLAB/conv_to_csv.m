2649551256b7f54b5ffa59defe92f6525efe3236
abhishekkhare1998 <abhishek.n.khare@gmail.com>
2022-04-30 18:46:46 -0700

    Added base, stimuli and metadata

--- MATLAB/conv_to_csv.m
+++ MATLAB/conv_to_csv.m
@@ -1,8 +1,18 @@
 dreamer_mat_file = [pwd '\DREAMER.mat'];
 file_data = load(dreamer_mat_file);
+
 for i = 1:23
+    valence_scores = file_data.DREAMER.Data{1, i}.ScoreValence;
+    arousal_scores = file_data.DREAMER.Data{1, i}.ScoreArousal;
+    dominance_scores = file_data.DREAMER.Data{1, i}.ScoreDominance;
+    electrodes = [1:18]';
+    metadata_matrix = [electrodes valence_scores arousal_scores dominance_scores];
     for j = 1:18
-        name = [pwd '\..\Python\Data' '\Dreamer_' num2str(i) '_' num2str(j) '.csv'];
-        csvwrite(name, file_data.DREAMER.Data{1,i}.EEG.baseline{j,1});
+        name_base = [pwd '\..\Python\Data\Baseline' '\Dreamer_' num2str(i) '_' num2str(j) '.csv'];
+        name_stimuli = [pwd '\..\Python\Data\Stimuli' '\Dreamer_' num2str(i) '_' num2str(j) '.csv'];
+        name_meta = [pwd '\..\Python\Data\Metadata' '\Dreamer_VAD_' num2str(i) '.csv'];
+        csvwrite(name_base, file_data.DREAMER.Data{1,i}.EEG.baseline{j,1});
+        csvwrite(name_stimuli, file_data.DREAMER.Data{1,i}.EEG.stimuli{j,1});
+        csvwrite(name_meta, metadata_matrix);
     end
 end
