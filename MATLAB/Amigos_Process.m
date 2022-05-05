76a7548c584791acc451b24fd5d0e4325d57e6f5
g8singh <95119347+g8singh@users.noreply.github.com>
2022-05-04 21:24:19 -0700

    Add files via upload

--- MATLAB/Amigos_Process.m
+++ MATLAB/Amigos_Process.m
@@ -1,25 +1,29 @@
 per_user = [];
+per_user_label = [];
 
 for i=1:40
 
     per_emotion = [];
+    per_emotion_label = [];
 
-    filepath = "Data_Original_P"+ num2str(i,'%02d') + "/Data_Original_P" + num2str(i,'%02d') + ".mat";
+    filepath = "Data_Preprocessed_P"+ num2str(i,'%02d') + "/Data_Preprocessed_P" + num2str(i,'%02d') + ".mat";
 
     data = load(filepath);
 
-    EEG = data.EEG_DATA;
+    EEG = data.joined_data;
 
     for j = 1:20
         
         EEG_Emotion = EEG{j};
 
+        label_Emotion = data.labels_selfassessment{j};
+
         per_emotion_electrode = [];
 
-        if(size(EEG_Emotion,1) == 0)
+        if((sum(isnan(EEG_Emotion),'all') > 0) || (size(EEG_Emotion,1) == 0) || (size(label_Emotion,1) == 0))
             per_emotion_electrode = zeros(14,3);
         else
-            for k = 4:17    
+            for k = 1:14    
 
                 EEG_Emotion_Electrode = EEG_Emotion(:,k);
                 theta = bandpower(EEG_Emotion_Electrode,128,[4 7]);
@@ -27,16 +31,18 @@ for i=1:40
                 beta  = bandpower(EEG_Emotion_Electrode,128,[13 30]);
                 per_emotion_electrode = [per_emotion_electrode; theta alpha beta];
             end
+
+            per_emotion_electrode = reshape(per_emotion_electrode,1,[]);
+            per_emotion = [per_emotion; per_emotion_electrode];
+            per_emotion_label = [per_emotion_label; label_Emotion];
+
         end
-        
-        per_emotion_electrode = reshape(per_emotion_electrode,1,[]);
-
-        per_emotion = [per_emotion; per_emotion_electrode];
-
     end
 
     per_user = [per_user; per_emotion];
+    per_user_label = [per_user_label; per_emotion_label];
 
 end
 
 save('all_features_amigos.mat',"per_user");
+save('labels_amigos.mat',"per_user_label");
