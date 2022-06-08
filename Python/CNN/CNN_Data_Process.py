d4914d6b02d5577535d3f2300e8e552f0e1ad0ad
g8singh <95119347+g8singh@users.noreply.github.com>
2022-06-08 12:01:05 -0700

    Add files via upload

--- Python/CNN/CNN_Data_Process.py
+++ Python/CNN/CNN_Data_Process.py
@@ -9,27 +9,19 @@ Created on Mon May 30 22:05:46 2022
 # General libraries
 import pandas as pd  #For working with dataframes
 import numpy as np   #For working with image arrays
-import cv2          #For transforming image
 
 #For model building
 import torch
-import torchvision
-from torchvision import transforms, datasets, models, utils
-from torch.utils.data import Dataset, DataLoader 
-from PIL import Image
+from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
-from torch.nn import functional as F
-from skimage import io, transform
-from torch.optim import lr_scheduler
-from skimage.transform import AffineTransform, warp
 
 
 class MyData(Dataset):
     def __init__(self, train=True, transform=None):
         #Loading train.csv
-        train_df=pd.read_csv('img_train.csv')
+        train_df=pd.read_csv('Img_Data.csv')
         #Loading image data and merging with train.csv
-        df = pd.read_csv('img_labels.csv')
+        df = pd.read_csv('Images_Class.csv')
         #Leaving only image related  columns
         feature=train_df
         #Setting labels
@@ -67,87 +59,4 @@ class MyData(Dataset):
             sample=self.transform(sample)
             
         return sample
-    
-class crop(object):
-    def __init__(self, resize_size):
-        self.resize_size = resize_size
-    def __call__(self, sample):
-        image, label1, label2 = sample['image'],\
-        sample['label_valence'], sample['label_arousal']
-        _, thresh=cv2.threshold(image, 30, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        contours, _ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-        
-        idx=0
-        ls_xmin=[]
-        ls_ymin=[]
-        ls_xmax=[]
-        ls_ymax=[]
-        
-        for cnt in contours:
-            idx+=1
-            x,y,w,h = cv2.boundingRect(cnt)
-            ls_xmin.append(x)
-            ls_ymin.append(y)
-            ls_xmax.append(x + w)
-            ls_ymax.append(y + h)
-        xmin = min(ls_xmin)
-        ymin = min(ls_ymin)
-        xmax = max(ls_xmax)
-        ymax = max(ls_ymax)
-        roi = image[ymin:ymax,xmin:xmax]    
-        resized_image = cv2.resize(roi, (self.resize_size, self.resize_size),\
-                                                             interpolation=cv2.INTER_AREA)
-        sample={'image': resized_image, 'label_valence': label1, 'label_arousal': label2}
-        return sample
-
-class rotate_image(object):
-    def __call__(self, sample):
-        image, label1, label2 = sample['image'],\
-        sample['label_valence'], sample['label_arousal']
-        min_scale = 0.8
-        max_scale = 1.2
-        sx = np.random.uniform(min_scale, max_scale)
-        sy = np.random.uniform(min_scale, max_scale)
-        # --- rotation ---
-        max_rot_angle = 7
-        rot_angle = np.random.uniform(-max_rot_angle, max_rot_angle) * np.pi / 180.
-        # --- shear ---
-        max_shear_angle = 10
-        shear_angle = np.random.uniform(-max_shear_angle, max_shear_angle) * np.pi / 180.
-        # --- translation ---
-        max_translation = 4
-        tx = np.random.randint(-max_translation, max_translation)
-        ty = np.random.randint(-max_translation, max_translation)
-        tform = AffineTransform(scale=(sx, sy), rotation=rot_angle, shear=shear_angle,
-                            translation=(tx, ty))
-        transformed_image = warp(image, tform)
-        assert transformed_image.ndim == 2
-        sample={'image': transformed_image, 'label_valence': label1, 'label_arousal': label2}
-        return sample
-
-class RGB_ToTensor(object):
-    def __call__(self, sample):
-        image, label1, label2 = sample['image'],\
-        sample['label_valence'], sample['label_arousal']
-        
-        image=torch.from_numpy(image).unsqueeze_(0).repeat(3, 1, 1)
-        label1=torch.from_numpy(label1)
-        label2=torch.from_numpy(label2)
-        
-        return {'image': image,
-                'label_valence': label1,
-                'label_arousal': label2}
-
-class Normalization(object):
-    def __init__(self, mean, std):
-        self.mean = mean.view(-1, 1, 1)
-        self.std = std.view(-1, 1, 1)
-    def __call__(self, sample):
-        image, label1, label2 = sample['image'],\
-        sample['label_valence'], sample['label_arousal']
-        
-        return {'image': image,
-                'label_valence': label1,
-                'label_arousal': label2}
-
     
