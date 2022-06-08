7439af2ffdf52815ab152eb57e9a4bd8bac1bb67
abhishekkhare1998 <abhishek.n.khare@gmail.com>
2022-06-08 06:13:59 -0700

    test2

--- Python/Main/StatsClassification.py
+++ Python/Main/StatsClassification.py
@@ -80,7 +80,7 @@ def calculate_accuracy(test_labels, test_predictions, method, is_valence):
 def run_main():
     dataset_used = "amigos"  # "amigos" or "dreamer"
     current_path = os.path.realpath(__file__).rsplit("\\", 1)[0]
-    dataset_folder_path = os.path.join(current_path, "..", "Data", "Extracted_features")
+    dataset_folder_path = os.path.join(current_path.rsplit("\\", 1)[0], "Data", "Extracted_features")
     dataset_path = dataset_folder_path + r"\\" + dataset_used + ".csv"
     labels_path = dataset_folder_path + r"\\" + dataset_used + "_labels.csv"
     input_data = pd.read_csv(dataset_path, header=None)
