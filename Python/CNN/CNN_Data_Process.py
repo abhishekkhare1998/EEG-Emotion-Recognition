c2e91e870cf310e0c9bfa243397a6ed72953275e
abhishekkhare1998 <abhishek.n.khare@gmail.com>
2022-06-09 19:08:36 -0700

    fixed PEP-8 violations

--- Python/CNN/CNN_Data_Process.py
+++ Python/CNN/CNN_Data_Process.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 30 22:05:46 2022
-
 @author: gauravsingh
 """
 
 # General libraries
-import pandas as pd  #For working with dataframes
-import numpy as np   #For working with image arrays
+import numpy as np
 import mat73 as mt
 import torch
 from torch.utils.data import Dataset
@@ -19,51 +17,51 @@ class MyData(Dataset):
     
     # Setting up the dataset
     def __init__(self, train=True, transform=None):
-        
-        #Loading Img_Data.csv
+
         train_df=mt.loadmat("all_img_amigos.mat")
         feature = np.array(train_df['Img_Cell'], dtype='uint8')
-        feature = np.uint8(np.reshape(feature,[feature.shape[0],3,128,128]))
-        #Loading Images labels from Images_Classes.csv
+        feature = np.uint8(np.reshape(feature, [feature.shape[0], 3, 128, 128]))
+
+        # Loading Images labels from Images_Classes.csv
         df = mt.loadmat('Images_Class.mat')
-        #Leaving only image related  columns
+
+        # Leaving only image related  columns
         label_int = np.array(df['k'], dtype='uint8')
         
-        #Setting labels
-        label_valence=label_int[:,0]
-        label_arousal=label_int[:,1]
+        # Setting labels
+        label_valence = label_int[:, 0]
+        label_arousal = label_int[:, 1]
         
-        #Splitting the data into train and validation set
+        # Splitting the data into train and validation set
         X_train, X_test, y_valence_train, y_valence_test, y_arousal_train, y_arousal_test\
         = train_test_split(feature, label_valence, label_arousal, test_size=0.2)
         
-        if train==True:
-            self.x=X_train
-            self.valence_y=y_valence_train
-            self.arousal_y=y_arousal_train
+        if train:
+            self.x = X_train
+            self.valence_y = y_valence_train
+            self.arousal_y = y_arousal_train
         else:
-            self.x=X_test
-            self.valence_y=y_valence_test
-            self.arousal_y=y_arousal_test            
+            self.x = X_test
+            self.valence_y = y_valence_test
+            self.arousal_y = y_arousal_test
         
-        #Applying transformation
-        self.transform=transform
+        # Applying transformation
+        self.transform = transform
         
     def __len__(self):
         return len(self.x)
     
-    #Getting the item from the dataset
+    # Getting the item from the dataset
     def __getitem__(self, idx):
-        image=torch.from_numpy(self.x[idx]).type(torch.ByteTensor)
-        label1=self.valence_y[idx]
-        label2=self.arousal_y[idx]
+        image = torch.from_numpy(self.x[idx]).type(torch.ByteTensor)
+        label1 = self.valence_y[idx]
+        label2 = self.arousal_y[idx]
         
-        sample={'image': image, 'label_valence': label1,\
-                'label_arousal': label2}
+        sample = {'image': image, 'label_valence': label1,\
+                  'label_arousal': label2}
         
-        #Applying transformation
+        # Applying transformation
         if self.transform:
             sample=self.transform(sample)
             
         return sample
-    
--- Python/CNN/CNN_Model.py
+++ Python/CNN/CNN_Model.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 30 22:14:59 2022
-
 @author: gauravsingh
 """
 
@@ -19,27 +18,27 @@ from torch.nn import functional as F
 
 
 class CNN1(nn.Module):
-    #Setup the architecture of rhe CNN
+    # Setup the architecture of rhe CNN
     def __init__(self, pretrained):
         super(CNN1, self).__init__()
-        self.conv1 = nn.Conv2d(3, 8, 16)       #First Convolutional filter with size 16x16x8
-        self.conv2 = nn.Conv2d(8, 8, 8)        #Second Convolutional filter with size 8x8x8
-        self.fc1 = nn.Linear(3872, 256)        #Fully Connected Layer
-        self.pool1 = nn.MaxPool2d(8, stride=2) #Max Pool layer with stride as 2 and filter size as 8x8   
-        self.pool2 = nn.MaxPool2d(4, stride=2) #Max Pool layer with stride as 2 and filter size as 4x4        
-        self.fc21 = nn.Linear(256, 3)          #Output layer for valence class
-        self.fc22 = nn.Linear(256, 3)          #Output layer for arousal class
-        
-        
-    #Defining forward pass in the model    
+        self.conv1 = nn.Conv2d(3, 8, 16)       # First Convolutional filter with size 16x16x8
+        self.conv2 = nn.Conv2d(8, 8, 8)        # Second Convolutional filter with size 8x8x8
+        self.fc1 = nn.Linear(3872, 256)        # Fully Connected Layer
+        self.pool1 = nn.MaxPool2d(8, stride=2) # Max Pool layer with stride as 2 and filter size as 8x8
+        self.pool2 = nn.MaxPool2d(4, stride=2) # Max Pool layer with stride as 2 and filter size as 4x4
+        self.fc21 = nn.Linear(256, 3)          # Output layer for valence class
+        self.fc22 = nn.Linear(256, 3)          # Output layer for arousal class
+
+    # Defining forward pass in the model
+
     def forward(self, x):
         
-        #First Set of Convolutional layer with ReLU activation
+        # First Set of Convolutional layer with ReLU activation
         x = self.conv1(x) 
         x = F.relu(x)              
         x = self.pool1(x)
         
-        #Second Set of Convolutional layer with ReLU activation
+        # Second Set of Convolutional layer with ReLU activation
         x = self.conv2(x)
         x = F.relu(x)
         x = self.pool2(x)
@@ -49,32 +48,35 @@ class CNN1(nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         
-        #Output layer for the first label
+        # Output layer for the first label
         label1 = self.fc21(x)
         label1 = F.log_softmax(label1, dim=1)
 
-        #Output layer for the second label
+        # Output layer for the second label
         label2 = self.fc22(x)
         label2 = F.log_softmax(label2, dim=1)
         return {'label1': label1, 'label2': label2}
 
+# Setting model and moving to device incase of GPU
 
-#Setting model and moving to device incase of GPU
+
 model_CNN = CNN1(True)
 
-#For multilabel output: Valence and Arousal
+# For multilabel output: Valence and Arousal
 criterion_multioutput = nn.CrossEntropyLoss()
 
-#Setup of the optimizer
+# Setup of the optimizer
 optimizer = optim.SGD(model_CNN.parameters(), lr=0.001, momentum=0.9)
 
-#Defining the training ofthe model
+# Defining the training ofthe model
+
+
 def train_model(model, criterion1, optimizer, n_epochs=100):
     """returns trained model"""
     # initialize tracker for minimum validation loss
     valid_loss_min = np.Inf
     
-    #Running it for 100 epochs
+    # Running it for 100 epochs
     for epoch in range(1, n_epochs):
         train_loss = 0.0
         valid_loss = 0.0
@@ -87,14 +89,14 @@ def train_model(model, criterion1, optimizer, n_epochs=100):
                                               sample_batched['label_arousal'] 
             # zero the parameter gradients
             optimizer.zero_grad()
-            output=model(image)
-            label1_hat=output['label1']
-            label2_hat=output['label2']
+            output = model(image)
+            label1_hat = output['label1']
+            label2_hat = output['label2']
             
             # calculate loss of training data
-            loss1=criterion1(label1_hat, label1.squeeze().type(torch.LongTensor))
-            loss2=criterion1(label2_hat, label2.squeeze().type(torch.LongTensor))    
-            loss=loss1+loss2
+            loss1 = criterion1(label1_hat, label1.squeeze().type(torch.LongTensor))
+            loss2 = criterion1(label2_hat, label2.squeeze().type(torch.LongTensor))
+            loss = loss1+loss2
             
             # back prop
             loss.backward()
@@ -107,20 +109,20 @@ def train_model(model, criterion1, optimizer, n_epochs=100):
                 print('Epoch %d, Batch %d loss: %.6f' %
                   (epoch, batch_idx + 1, train_loss))
                 
-        # validate the model #
+        # validate the model
         model.eval()
         for batch_idx, sample_batched in enumerate(test_dataloader):
             image, label1, label2 = sample_batched['image'],\
                                              sample_batched['label_valence'],\
                                               sample_batched['label_arousal']
-            output=model(image)
-            label1_hat=output['label1']
-            label2_hat=output['label2']
+            output = model(image)
+            label1_hat = output['label1']
+            label2_hat = output['label2']
               
             # calculate loss of testing data
-            loss1=criterion1(label1_hat, label1.squeeze().type(torch.LongTensor))
-            loss2=criterion1(label2_hat, label2.squeeze().type(torch.LongTensor))
-            loss=loss1+loss2
+            loss1 = criterion1(label1_hat, label1.squeeze().type(torch.LongTensor))
+            loss2 = criterion1(label2_hat, label2.squeeze().type(torch.LongTensor))
+            loss = loss1+loss2
             
             valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - valid_loss))
         
@@ -128,7 +130,7 @@ def train_model(model, criterion1, optimizer, n_epochs=100):
         print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
             epoch, train_loss, valid_loss))
         
-        ## TODO: save the model if validation loss has decreased
+        # save the model if validation loss has decreased
         if valid_loss < valid_loss_min:
             torch.save(model, 'model.pt')
             print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
@@ -140,10 +142,12 @@ def train_model(model, criterion1, optimizer, n_epochs=100):
     return model
 
 # Processing and storing the data
+
+
 transformed_train_data = MyData(train=True, transform=None)
 transformed_test_data = MyData(train=False, transform=None)
 train_dataloader = DataLoader(transformed_train_data, batch_size=50, shuffle=True, num_workers=4)
 test_dataloader = DataLoader(transformed_test_data, batch_size=50, shuffle=True, num_workers=4)
 
 # Training the model
-model_conv=train_model(model_CNN, criterion_multioutput, optimizer)
+model_conv = train_model(model_CNN, criterion_multioutput, optimizer)
